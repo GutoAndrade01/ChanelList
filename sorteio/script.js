3d5cbedbf2ad79be08dfbe25fe2e4fa6a326ef70
GutoAndrade01 <92386662+GutoAndrade01@users.noreply.github.com>
2023-08-25 14:25:04 -0300

    sistema de sorteio

---
+++ sorteio/script.js
@@ -0,0 +1,60 @@
+let numerosSorteados = [];
+let numeroAtual;
+let contador = 0;
+
+function sortearNumero() {
+  const minValue = parseInt(document.getElementById("minValue").value);
+  const maxValue = parseInt(document.getElementById("maxValue").value);
+
+  if (minValue >= maxValue) {
+    alert("O valor mínimo deve ser menor que o valor máximo!");
+    return;
+  }
+
+  numeroAtual = gerarNumeroUnico(minValue, maxValue);
+  numerosSorteados.push(numeroAtual);
+  contador++;
+  exibirNumeroAtual();
+  exibirNumerosSorteados();
+  exibirContador();
+}
+
+function gerarNumeroUnico(min, max) {
+  let novoNumero;
+
+  do {
+    novoNumero = Math.floor(Math.random() * (max - min + 1)) + min;
+  } while (numerosSorteados.includes(novoNumero));
+
+  return novoNumero;
+}
+
+function exibirNumeroAtual() {
+  const currentNumberElement = document.getElementById("currentNumber");
+  currentNumberElement.textContent = numeroAtual;
+}
+
+function exibirNumerosSorteados() {
+  const historyGrid = document.getElementById("historyGrid");
+  historyGrid.innerHTML = "";
+
+  numerosSorteados.forEach(numero => {
+    const div = document.createElement("div");
+    div.textContent = numero;
+    historyGrid.appendChild(div);
+  });
+}
+
+function exibirContador() {
+  const counterElement = document.getElementById("counter");
+  counterElement.textContent = contador;
+}
+
+function limparTudo() {
+  numerosSorteados = [];
+  numeroAtual = null;
+  contador = 0;
+  exibirNumeroAtual();
+  exibirNumerosSorteados();
+  exibirContador();
+}
